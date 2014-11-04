472f083e81ff3eb150ea34446f920d45ffa3e7cc
Sameer Sah <sahsam07@gmail.com>
2014-11-04 22:41:09 +0530

    create index.php
    first page of project

---
+++ index.php
@@ -0,0 +1,62 @@
+<html >
+<head>
+<title>|| VOTING SYSTEM ||</title>
+<link href="design.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+</style>
+</head>
+<body>
+<div id="header">
+ <img src="images/logo1.jpg" alt="Vote" width="" height="" style="paddimg:0px" /> 
+</div>
+<br><br>
+<div id="menu">
+	<ul>
+		<li> <a href="index.php"> |  Home  |</a></li>
+		<li>
+		  <a href="registration.php">|  Registration  |</a></li>
+		<li>
+		  <a href="login.php" >|  Login  |</a></li>
+		
+		
+		<li>
+		  <a href="result.php">|  Result  |</a></li>
+		
+		<li>
+		  <a href="contact.php">|  Contact Us  |</a></li>
+	</ul>
+</div>
+<br><br><br><br>
+<div id="content">
+  <table width="" align="center">
+      <tr>
+        <td height=""><img src="images/ballot.jpg" alt="Vote" width="450" height="350" /></td>
+      </tr>
+    </table>
+	<p>&nbsp;</p>
+</div>
+</div>
+<div id="footer">
+	<table width="490" align="center">
+      <tr>
+        <td width="" height=""><p class="style1">WELCOME  </p>
+          <p width="450"align="justify">You  are welcome to the  Electronic voting system .Scared of waiting in lines ? Got no time to vote? Dont worry.
+		  Electronic voting is any form of  vote collection involving electronic usually computer device.
+		  The  electronic processes of voter registration, vote casting , vote  counting and ballot generating are becoming increasingly electronic and  automated .</p>
+        <p align="justify">At last the Electronic system is introduced which is open,  
+		but provides for secret and secure voting and can be used and verified  over existing network system</p></td>
+        <td width="93"><img src="" alt=""  /></td>
+      </tr>
+      <tr>
+        <td height="17">&nbsp;</td>
+        <td></td>
+      </tr>
+    </table>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<p></a></p>
+</div>
+</body>
+</html>
+
+
