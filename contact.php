847983a13c33db2c2c48a7ce314ccba46f110e04
Sameer Sah <sahsam07@gmail.com>
2014-11-04 22:48:12 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link href="design.css" rel="stylesheet" type="text/css" />
+</head>
+<body align="center" style="color:white">
+<div id="header">
+ <img src="images/logo1.jpg" align="left"alt="Vote" width="" height="" style="paddimg:0px" /> 
+</div>
+<br><br>
+<h1>Created By :</h2><br><br>
+<h3>Sameer Sah<br>11BCE0252<br><br>Arihant Pawariya<br>11BCEXXX<br><br><br><br>For any query-<br><br>phno: 8220377044<br>Email: sameer2011@vit.ac.in</h3>
+
+</body>
+</html>
